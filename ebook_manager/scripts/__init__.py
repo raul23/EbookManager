c07c4566605921e97869f7a4fb2890375bd12cb3
Raul <rchfe23@gmail.com>
2020-05-02 15:22:36 -0400

    Comments

--- ebook_manager/scripts/__init__.py
+++ ebook_manager/scripts/__init__.py
@@ -2,6 +2,7 @@
 
 """
 
-# TODO: don't install here but in the corresponding modules
+# TODO: explain why we install here. The loggers created before won't have the
+# correct logger class
 from pyutils import install_colored_logger
 install_colored_logger()

e66b3a4f1bafc90201eecbb16fa3de6e07aae5d4
Raul <rchfe23@gmail.com>
2020-05-08 16:36:05 -0400

    Started model

--- ebook_manager/webapp/ebooks/models.py
+++ ebook_manager/webapp/ebooks/models.py
@@ -1,3 +1,24 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    series = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    # TODO:
+    # - For isbn10 and isbn13, try to use a MaxValueValidator instead of a
+    # CharField as explained in https://stackoverflow.com/a/30850101
+    #
+    # - Also test with PositiveIntegerField, though django doc says that 'Values
+    # from 0 to 2147483647 are safe in all databases supported by Django'
+    # Ref.: https://bit.ly/2YGsxAI
+    isbn10 = models.CharField(max_length=10,
+                              validators=[RegexValidator(r'^\d{1,10}$')])
+    isbn13 = models.CharField(max_length=13,
+                              validators=[RegexValidator(r'^\d{1,10}$')])
+    pub_date = models.DateTimeField('Date published')
+
+
+class Author(models.Model):
+    books = models.ManyToManyField(Book)

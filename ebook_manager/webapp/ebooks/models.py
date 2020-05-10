b112cc067878fd4ce2c38251d22b0a686861e301
Raul <rchfe23@gmail.com>
2020-05-10 00:13:05 -0400

    - Add default, blank and null args to some of the fields - Add class Meta to Category model to fix plural name - Add class Meta to Tag model to use combination of two variables as primary key - Add choices to Rating and Tag models

--- ebook_manager/webapp/ebooks/models.py
+++ ebook_manager/webapp/ebooks/models.py
@@ -1,12 +1,18 @@
-from django.core.validators import RegexValidator
+from decimal import Decimal
+
+from django.core.validators import (MinValueValidator, MaxValueValidator,
+                                    RegexValidator)
 from django.db import models
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    series = models.CharField(max_length=200, default="")
-    publisher = models.CharField(max_length=200, default="")
-    pub_date = models.DateTimeField('Date published', default=None)
+    series = models.CharField(max_length=200, default="", blank=True)
+    publisher = models.CharField(max_length=200, default="", blank=True)
+    pub_date = models.DateTimeField('Date published',
+                                    default=None,
+                                    blank=True,
+                                    null=True)
     # TODO:
     # - For isbn10 and isbn13, try to use a MaxValueValidator instead of a
     # CharField as explained in https://stackoverflow.com/a/30850101
@@ -16,31 +22,35 @@ class Book(models.Model):
     # Ref.: https://bit.ly/2YGsxAI
     # TODO: isbn should be unique
     isbn10 = models.CharField(max_length=10,
-                              validators=[RegexValidator(r'^\d{1,10}$')])
+                              validators=[RegexValidator(r'^\d{1,10}$')],
+                              default="",
+                              blank=True)
     isbn13 = models.CharField(max_length=13,
-                              validators=[RegexValidator(r'^\d{1,10}$')])
-    pages = models.IntegerField(default=None)
+                              validators=[RegexValidator(r'^\d{1,10}$')],
+                              default="",
+                              blank=True)
+    pages = models.IntegerField(default=None, blank=True, null=True)
     # Size is given in multiples of bytes, and the unit symbol is shown beside
     # the file size, e.g. 660 KB
-    size = models.CharField(max_length=10, default="")
+    size = models.CharField(max_length=10, default="", blank=True)
     # Book format, i.e. extension. For example, PDF, EPUB, or MOBI
-    format = models.CharField(max_length=10, default="")
+    format = models.CharField(max_length=10, default="", blank=True)
     # md5 and sha256 are in hexadecimal
     # TODO: md5 and sha256 should be unique
-    md5 = models.CharField(max_length=32, default="")
-    sha256 = models.CharField(max_length=64, default="")
+    md5 = models.CharField(max_length=32, default="", blank=True)
+    sha256 = models.CharField(max_length=64, default="", blank=True)
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
     # TODO: ImageField required the Pillow library
-    thumbnail_cover_image = models.ImageField(default=None)
-    enlarged_cover_image = models.ImageField(default=None)
+    thumbnail_cover_image = models.ImageField(default=None, blank=True)
+    enlarged_cover_image = models.ImageField(default=None, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Author(models.Model):
-    books = models.ManyToManyField(Book, default=None)
+    books = models.ManyToManyField(Book, blank=True)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     add_date = models.DateTimeField('Date added', auto_now_add=True)
@@ -51,7 +61,7 @@ class Author(models.Model):
 
 
 class Category(models.Model):
-    books = models.ManyToManyField(Book, default=None)
+    books = models.ManyToManyField(Book, blank=True)
     # TODO: category should be unique
     category = models.CharField(max_length=200)
     # TODO: add choices such as amazon, goodreads, personal
@@ -59,34 +69,57 @@ class Category(models.Model):
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.category
 
 
 class Rating(models.Model):
+
+    class SourceOfRating(models.TextChoices):
+        AMAZON = 'A'
+        GOODREADS = 'G'
+        PERSONAL = 'P'
+
     # TODO: primary key is (book_id, source)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     # TODO: add choices such as amazon, goodreads
-    source = models.CharField('Source of rating', max_length=200)
+    source = models.CharField('Source of rating',
+                              max_length=200,
+                              primary_key=True)
     # TODO: FloatField instead of DecimalField?,
     # see https://docs.djangoproject.com/en/3.0/ref/models/fields/#floatfield
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(Decimal('1')),
+                    MaxValueValidator(Decimal('5'))])
     nb_ratings = models.PositiveIntegerField('Number of ratings')
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
 
     def __str__(self):
-        return self.rating
+        return "{}: {}/5 [{}]".format(self.book.title, self.rating, self.source)
 
 
 class Tag(models.Model):
-    books = models.ManyToManyField(Book, default=None)
-    # TODO: tag should be unique
+
+    class SourceOfTag(models.TextChoices):
+        AMAZON = 'A'
+        GOODREADS = 'G'
+        PERSONAL = 'P'
+
+    class Meta:
+        unique_together = (("tag", "source"),)
+
+    books = models.ManyToManyField(Book, blank=True)
     tag = models.CharField(max_length=200)
     # TODO: add choices such as amazon, personal
-    source = models.CharField('Source of tag', max_length=200)
+    source = models.CharField('Source of tag',
+                              max_length=200,
+                              choices=SourceOfTag.choices)
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
 
     def __str__(self):
-        return self.tag
+        return "{} [{}]".format(self.tag, self.get_source_display())

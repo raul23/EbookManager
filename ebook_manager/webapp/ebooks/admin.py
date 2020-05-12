dbd024fce652419657627cffc3c88e6211299a5d
Raul <rchfe23@gmail.com>
2020-05-12 14:13:05 -0400

    Register BookFile in admin

--- ebook_manager/webapp/ebooks/admin.py
+++ ebook_manager/webapp/ebooks/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author, Book, Category, Rating, Tag
+from .models import Author, Book, BookFile, Category, Rating, Tag
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(BookFile)
 admin.site.register(Category)
 admin.site.register(Rating)
 admin.site.register(Tag)

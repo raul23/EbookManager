0145a5dd08b0d4b574ea00acb23b692f1ccb9305
Raul <rchfe23@gmail.com>
2020-05-13 13:31:03 -0400

    urls.py: use app_name

--- ebook_manager/webapp/ebooks/urls.py
+++ ebook_manager/webapp/ebooks/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path
 
-from . import views
+from . import apps, views
 
 
+app_name = apps.EbooksConfig.name
 urlpatterns = [
     # ex: /ebooks/
+    # the 'name' value as called by the {% url %} template tag
     path('', views.index, name='index'),
-    path('ebooks/', views.index, name='index'),
+    path('{}/'.format(app_name), views.index, name='index'),
     # ex: /ebooks/123456790/
-    path('<slug:book_id>/', views.book_detail, name='detail'),
+    path('{}/<slug:book_id>/'.format(app_name),
+         views.book_detail,
+         name='book_detail'),
     # ex: /ebooks/123456790/ratings/
-    path('<slug:book_id>/ratings/', views.book_ratings, name='ratings'),
+    path('{}/<slug:book_id>/ratings/'.format(app_name),
+         views.book_ratings,
+         name='book_ratings'),
     # ex: /ebooks/123456790/rate/
-    path('<slug:book_id>/rate/', views.rate, name='rate'),
+    path('{}/<slug:book_id>/rate/'.format(app_name), views.rate, name='rate'),
 ]

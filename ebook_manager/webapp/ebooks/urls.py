14c34b3ef370e5d2e094f60755c127cd74aaecb4
Raul <rchfe23@gmail.com>
2020-05-12 20:16:21 -0400

    Add urlpatterns for new views

--- ebook_manager/webapp/ebooks/urls.py
+++ ebook_manager/webapp/ebooks/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
+    # ex: /ebooks/
     path('', views.index, name='index'),
+    path('ebooks/', views.index, name='index'),
+    # ex: /ebooks/123456790/
+    path('<slug:book_id>/', views.book_detail, name='detail'),
+    # ex: /ebooks/123456790/ratings/
+    path('<slug:book_id>/ratings/', views.book_ratings, name='ratings'),
+    # ex: /ebooks/123456790/rate/
+    path('<slug:book_id>/rate/', views.rate, name='rate'),
 ]

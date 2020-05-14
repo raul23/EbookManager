a9a99ddc93ef61d64fa010f743ab24d069b78988
Raul <rchfe23@gmail.com>
2020-05-14 00:08:42 -0400

    rate(): begin code for processing POST data

--- ebook_manager/webapp/ebooks/views.py
+++ ebook_manager/webapp/ebooks/views.py
@@ -21,4 +21,25 @@ def book_ratings(request, book_id):
 
 
 def rate(request, book_id):
-    return HttpResponse("You're rating the book %s." % book_id)
+    import ipdb
+    ipdb.set_trace()
+    book = get_object_or_404(Book, pk=book_id)
+    try:
+        rating = request.POST['rating']
+    except (KeyError, Book.DoesNotExist):
+        # Redisplay the book rating form.
+        """
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+        """
+        pass
+    else:
+        # selected_choice.votes += 1
+        # selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        pass

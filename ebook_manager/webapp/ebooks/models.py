3155620cf5b83eb1b0368bb248718e83a9c22869
Raul <rchfe23@gmail.com>
2020-05-18 18:06:47 -0400

    - Add validate_ebook_file(): called when checking a file path is valid - Add validate_positive_number(): checks that a number is positive and greater than 0 - Add AbstractBook(): common fields for models Book and BookFile - pub_year (used to be called pub_date) only takes a year (YYYY) as the publishing date - BookFile: changed relation with Book from Foreign key to ManToMany - Author: takes only a name - Rating: changed field rating to avg_rating and nb_rating is checked that it is at least equal to 1

--- ebook_manager/webapp/ebooks/models.py
+++ ebook_manager/webapp/ebooks/models.py
@@ -1,98 +1,185 @@
-from decimal import Decimal
+import os
+from datetime import datetime
 
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+from django.core.validators import (MinValueValidator, MaxValueValidator,
+                                    RegexValidator)
 from django.db import models
 
+from . import filtered_iso_639_languages
 
-class Book(models.Model):
 
-    class TypeOfBookId(models.TextChoices):
-        ISBN10 = 'I10'
-        ISBN13 = 'I13'
-        ASIN = 'A'
+def validate_ebook_file(filepath):
+    # Check that the file exists
+    if os.path.exists(filepath):
+        # Check that it is a file and not a directory
+        if os.path.isfile(filepath):
+            # Check that it is a valid ebook file, i.e. valid extension
+            # TODO: check scripts.tools._split_fname()
+            root, ext = os.path.splitext(filepath)
+            ext = ext[1:]
+            if ext not in BookFile.allowed_extensions:
+                # TODO: ugettext_lazy as _ (like in languages module)?
+                raise ValidationError(
+                    '{} is not a valid ebook file extension. Allowed '
+                    'extensions: {}'.format(ext, BookFile.allowed_extensions)
+                )
+        else:
+            raise ValidationError('Not a file: {}'.format(filepath))
+    else:
+        raise ValidationError("File doesn't exist: {}".format(filepath))
+
+
+def validate_positive_number(value):
+    if value <= 0:
+        raise ValidationError('Ensure this value is greater than 0.')
+
+
+class AbstractBook(models.Model):
+    # TODO: check if better way than repeat type
+    type_of_book_id = [(t, t) for t in ['ASIN', 'ISBN10', 'ISBN13']]
+
+    class Meta:
+        abstract = True
+
+    # Title is required
+    title = models.CharField(max_length=200)
+    # TODO:
+    # - identifiers should be unique; right now you could have two books with
+    #   the same book_id (e.g. ISBN)
+    # - get edition too
+    # - isbn10 can be converted to isbn13, see https://bit.ly/3bxdP1y [Wikipedia]
+    # - validate book identifiers
+    book_id = models.CharField('Book Identifier (e.g. ISBN-10 or ASIN)',
+                               max_length=20,
+                               default="",
+                               blank=True)
+    # TODO: based on type of book id, check that the book id is valid
+    book_id_type = models.CharField('Type of Book Identifier',
+                                    max_length=10,
+                                    default="",
+                                    blank=True,
+                                    choices=type_of_book_id)
+    series = models.CharField(max_length=200, default="", blank=True)
+    publisher = models.CharField(max_length=200, default="", blank=True)
+    pub_year = models.PositiveIntegerField(
+        'Year published',
+        default=None,
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(1000),
+                    MaxValueValidator(datetime.now().year + 1)],
+        help_text="Use the following format: YYYY")
+    # NOTE: isbn10, isbn, and asin are required and readonly (see admin.py)
+    #
+    # TODO:
+    # - For isbn10 and isbn13, try to use a MaxValueValidator instead of a
+    # CharField as explained in https://stackoverflow.com/a/30850101
+    #
+    # - Also test with PositiveIntegerField, though django doc says that 'Values
+    # from 0 to 2147483647 are safe in all databases supported by Django'
+    # Ref.: https://bit.ly/2YGsxAI
+    #
+    # - Test these fields' validators even though editable is False
+    isbn10 = models.CharField('ISBN-10',
+                              max_length=10,
+                              validators=[RegexValidator(r'^\d{1,10}$')],
+                              default="",
+                              blank=True)
+    isbn13 = models.CharField('ISBN-13',
+                              max_length=13,
+                              validators=[RegexValidator(r'^\d{1,10}$')],
+                              default="",
+                              blank=True)
+    # TODO: add validators for asin
+    asin = models.CharField('ASIN', max_length=10, default="", blank=True)
+    # pages > 0
+    pages = models.IntegerField(default=None,
+                                null=True,
+                                blank=True,
+                                validators=[validate_positive_number])
+    language = models.CharField(max_length=100,
+                                default="",
+                                blank=True,
+                                choices=filtered_iso_639_languages.LANGUAGES)
+    add_date = models.DateTimeField('Date added', auto_now_add=True)
+    update_date = models.DateTimeField('Date updated', auto_now=True)
+    # TODO: ImageField requires the Pillow library
+    # TODO: test images are loaded
+    thumbnail_cover_image = models.ImageField(default=None, blank=True)
+    enlarged_cover_image = models.ImageField(default=None, blank=True)
+
+
+class Book(AbstractBook):
 
     class BookFormat(models.TextChoices):
         Hardcover = 'H'
+        Kindle = 'K'
         Paperback = 'P'
 
-    # Primary key, e.g. ISBN-10 or ASIN
-    # TODO: change to id
-    book_id = models.CharField(max_length=13,
-                               default="",
-                               blank=True,
-                               primary_key=True)
-    # TODO: based on type of book id, check that the book id is valid
-    # Required field
-    id_type = models.CharField('Type of Book Id',
-                               max_length=10,
-                               choices=TypeOfBookId.choices)
-    # Required field
-    title = models.CharField(max_length=200)
-    series = models.CharField(max_length=200, default="", blank=True)
-    publisher = models.CharField(max_length=200, default="", blank=True)
-    pub_date = models.DateTimeField('Date published',
-                                    default=None,
-                                    blank=True,
-                                    null=True)
-    pages = models.IntegerField(default=None, blank=True, null=True)
-    language = models.CharField(max_length=50, default="", blank=True)
-    # Book format. For example, hardcover and paperback
+    # NOTE: Automatic primary key
+    # Book format is required
+    # For example, hardcover and paperback
     book_format = models.CharField(max_length=10,
                                    default="",
                                    blank=True,
                                    choices=BookFormat.choices)
-    add_date = models.DateTimeField('Date added', auto_now_add=True)
-    update_date = models.DateTimeField('Date updated', auto_now=True)
-    # TODO: ImageField required the Pillow library
-    thumbnail_cover_image = models.ImageField(default=None, blank=True)
-    enlarged_cover_image = models.ImageField(default=None, blank=True)
 
     def __str__(self):
-        return "{} [{}]".format(self.title, self.book_id)
+        if self.book_id:
+            return "{} [{}]".format(self.title, self.book_id)
+        else:
+            return self.title
 
 
-class BookFile(models.Model):
+class BookFile(AbstractBook):
+    # TODO: extensions already found in scripts/tools.py
+    allowed_extensions = ('azw', 'azw3', 'cbz', 'chm', 'djvu', 'docx', 'epub',
+                          'gz', 'mobi', 'pdf', 'rar', 'zip',)
 
     class Meta:
-        unique_together = (("book", "md5", "file_path"), ("md5", "file_path"),)
+        # TODO: test combinations
+        unique_together = (("md5", "file_path"),)
 
     # Automatic primary key
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    pages = models.IntegerField(default=None, blank=True, null=True)
+    # book is required field
+    # TODO: test on_delete
+    books = models.ManyToManyField(Book, blank=True)
     # Size is given in multiples of bytes, and the unit symbol is shown beside
     # the file size, e.g. 660 KB
-    size = models.CharField(max_length=10, default="", blank=True)
-    # File extension. For example, PDF, EPUB, or MOBI
-    extension = models.CharField(max_length=10, default="", blank=True)
-    # Required fields
+    # TODO: should be IntegerField
+    size = models.CharField('File size',
+                            max_length=10,
+                            default="MB",
+                            editable=False)
+    size_unit = models.CharField('Size unit',
+                                 max_length=2,
+                                 default="MB",
+                                 editable=False)
     # md5 and sha256 are in hexadecimal
-    md5 = models.CharField(max_length=32)
-    sha256 = models.CharField(max_length=64)
+    # TODO: validate these two fields
+    md5 = models.CharField('MD5', max_length=32, default="")
+    sha256 = models.CharField('SHA256', max_length=64, default="")
     # TODO: Should file_path be CharField with max_length (but what)?
-    # Required field
-    file_path = models.TextField()
-    add_date = models.DateTimeField('Date added', auto_now_add=True)
-    update_date = models.DateTimeField('Date updated', auto_now=True)
-    thumbnail_cover_image = models.ImageField(default=None, blank=True)
-    enlarged_cover_image = models.ImageField(default=None, blank=True)
+    # file_path is required field
+    file_path = models.TextField(validators=[validate_ebook_file])
 
     def __str__(self):
-        return "{} [{}]: {}".format(self.book.title,
-                                    self.book.book_id,
+        return "{} [{}]: {}".format(self.title,
+                                    self.book_id,
                                     self.file_path)
 
 
 class Author(models.Model):
-    # TODO: test primary key if automatic set
+    # TODO: test primary key if set automatic
     books = models.ManyToManyField(Book, blank=True)
     # Required fields
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
 
     def __str__(self):
-        return "{} {}".format(self.first_name, self.last_name)
+        return self.name
 
 
 class Category(models.Model):
@@ -110,10 +197,10 @@ class Category(models.Model):
     books = models.ManyToManyField(Book, blank=True)
     # TODO: check that category is unique
     category = models.CharField(max_length=200)
-    # TODO: add choices such as amazon, goodreads, personal
     source = models.CharField('Source of category',
                               max_length=200,
                               choices=SourceOfCategory.choices)
+    # Automatic fields
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
 
@@ -131,24 +218,28 @@ class Rating(models.Model):
     class Meta:
         unique_together = (("book", "source"),)
 
-    # TODO: primary key is (book_id, source)
+    # TODO: primary key is (book_id, source)?
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    # TODO: add choices such as amazon, goodreads
     source = models.CharField('Source of rating',
                               max_length=200,
                               choices=SourceOfRating.choices)
     # TODO: FloatField instead of DecimalField?,
     # see https://docs.djangoproject.com/en/3.0/ref/models/fields/#floatfield
-    rating = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(Decimal('1')),
-                    MaxValueValidator(Decimal('5'))])
-    nb_ratings = models.PositiveIntegerField('Number of ratings')
+    # 1 <= avg_rating <= 1
+    avg_rating = models.FloatField(
+        'Average rating',
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(5)])
+    # nb_ratings >= 0
+    nb_ratings = models.PositiveIntegerField('Number of ratings',
+                                             validators=[MinValueValidator(1)])
+    # Automatic fields
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
 
     def __str__(self):
         return "{}: {}/5, {} ratings [{}]".format(self.book.title,
-                                                  self.rating,
+                                                  self.avg_rating,
                                                   self.nb_ratings,
                                                   self.get_source_display())
 
@@ -170,6 +261,7 @@ class Tag(models.Model):
     source = models.CharField('Source of tag',
                               max_length=200,
                               choices=SourceOfTag.choices)
+    # Automatic fields
     add_date = models.DateTimeField('Date added', auto_now_add=True)
     update_date = models.DateTimeField('Date updated', auto_now=True)
 

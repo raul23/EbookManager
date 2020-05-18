1c852fc354e7a5981d6f31ccae20953bd3f3dc20
Raul <rchfe23@gmail.com>
2020-05-18 18:13:49 -0400

    Add test class: started only

--- ebook_manager/webapp/ebooks/tests.py
+++ ebook_manager/webapp/ebooks/tests.py
@@ -1,16 +1,12 @@
 from django.test import TestCase
 
 
-# Create your tests here.
-def debug_code():
-    from ebooks.models import Author, Book, Rating, Tag, Category
-    from django.utils import timezone
+class TestFunctions(TestCase):
+    # Create your tests here.
+    def test_clear_tb(self):
+        pass
 
-    b = Book(title="The Road", pub_date=timezone.now())
-    b.pages = 30
-    b.save()
+    def test_populate_db(self):
+        # Create and save books
+        pass
 
-    # Create authors
-    b.author_set.create(first_name="John", last_name="Doe")
-    b.author_set.create(first_name="James", last_name="King")
-    b.author_set.create(first_name="Francisco", last_name="Gomez")

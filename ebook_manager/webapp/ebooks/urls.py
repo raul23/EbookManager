e5f485a3ada9d8746e58430c7b944d09f92118dc
Raul <rchfe23@gmail.com>
2020-05-21 16:49:23 -0400

    Use generic views

--- ebook_manager/webapp/ebooks/urls.py
+++ ebook_manager/webapp/ebooks/urls.py
@@ -7,15 +7,16 @@ app_name = apps.EbooksConfig.name
 urlpatterns = [
     # ex: /ebooks/
     # the 'name' value as called by the {% url %} template tag
-    path('', views.index, name='index'),
-    path('{}/'.format(app_name), views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('{}/'.format(app_name), views.IndexView.as_view(), name='index'),
     # ex: /ebooks/123456790/
-    path('{}/<slug:book_id>/'.format(app_name),
-         views.book_detail,
+    # TODO: slug?
+    path('{}/<slug:pk>/'.format(app_name),
+         views.BookDetailView.as_view(),
          name='book_detail'),
     # ex: /ebooks/123456790/ratings/
-    path('{}/<slug:book_id>/ratings/'.format(app_name),
-         views.book_ratings,
+    path('{}/<slug:pk>/ratings/'.format(app_name),
+         views.BookRatingsView.as_view(),
          name='book_ratings'),
     # ex: /ebooks/123456790/rate/
     path('{}/<slug:book_id>/rate/'.format(app_name), views.rate, name='rate'),

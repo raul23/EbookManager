77a2b6c95edba4924bde0fff8fa756d483039823
Raul <rchfe23@gmail.com>
2020-05-28 01:01:54 -0400

    Add list of characters to be removed in filename to aoo config file

--- ebook_manager/webapp/ebooks/apps.py
+++ ebook_manager/webapp/ebooks/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
+# TODO: remove package import
+from ebooks.remove_chars_in_filename import LIST_CHARS
+
 
 class EbooksConfig(AppConfig):
     name = 'ebooks'
+    # TODO: remove package import
+    # remove_chars_in_filename = ['- chars to remove', ]
+    unwanted_chars = LIST_CHARS

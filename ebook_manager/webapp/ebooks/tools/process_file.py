392fad86ab4f1865f858ad7cdb5ca902cb6de924
Raul <rchfe23@gmail.com>
2020-06-03 19:00:49 -0400

    — __init__(): update chunk_size -  _get_hash(): add hash_factory param to generalize method to other hash algos - check_hash_in_db(): get md5 and sha256 for a given file

--- ebook_manager/webapp/ebooks/tools/process_file.py
+++ ebook_manager/webapp/ebooks/tools/process_file.py
@@ -10,21 +10,25 @@ from ebooks.apps import EbooksConfig
 class ProcessFile:
     def __init__(self, file):
         self.file = file
-        self.chunk_size = 8192
+        # Ref.: https://stackoverflow.com/a/11143944
+        self.chunk_size = 128 * hashlib.md5().block_size
         self.isbn = None
         self.asin = None
         self._unwanted_chars = EbooksConfig.unwanted_chars
 
-    def _get_hash(self):
-        # Ref.: https://stackoverflow.com/a/38719060
-        hash = hashlib.md5()
+    def _get_hash(self, hash_factory=hashlib.md5):
+        # Ref.:
+        # - https://stackoverflow.com/a/38719060
+        # - https://stackoverflow.com/a/4213255
+        hash = hash_factory()
         for chunk in self.file.chunks(chunk_size=self.chunk_size):
             hash.update(chunk)
         return hash.hexdigest()
 
     def check_hash_in_db(self):
         # Compute file's md5 and sha256
-        hash = self._get_hash()
+        md5_hash = self._get_hash()
+        sha256_hash = self._get_hash(hashlib.sha256)
         # Check if computed hash is already in db
 
     def get_isbn_from_filename(self):

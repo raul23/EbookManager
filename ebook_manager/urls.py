2c383476e9da18a3bfbaa219f6c5b316e95e09f2
Raul <rchfe23@gmail.com>
2020-09-22 01:29:54 -0400

    urls.py: get label from webapp ebook_manager and use it in building different URLs for views

--- ebook_manager/urls.py
+++ ebook_manager/urls.py
@@ -4,22 +4,24 @@ from . import apps, views
 
 
 app_name = apps.EbookManagerConfig.name
+label = apps.EbookManagerConfig.label
+# app_name = label
 urlpatterns = [
-    # ex: /ebook_manager/add_files/
-    path('{}/upload_files/'.format(app_name), views.upload_files, name='upload_files'),
-    # ex: /ebook_manager/
+    # ex: /ebook-manager/add_files/
+    path('{}/upload_files/'.format(label), views.upload_files, name='upload_files'),
+    # ex: /ebook-manager/
     # the 'name' value as called by the {% url %} template tag
     re_path(r'^$', views.IndexView.as_view(), name='index'),
-    re_path(r'^{}/$'.format(app_name), views.IndexView.as_view(), name='index'),
-    # ex: /ebook_manager/123456790/
+    re_path(r'^{}/$'.format(label), views.IndexView.as_view(), name='index'),
+    # ex: /ebook-manager/123456790/
     # TODO: slug?
-    path('{}/<slug:pk>/'.format(app_name),  # route
+    path('{}/<slug:pk>/'.format(label),  # route
          views.BookDetailView.as_view(),    # view
          name='book_detail'),
-    # ex: /ebook_manager/123456790/ratings/
-    path('{}/<slug:pk>/ratings/'.format(app_name),  # route
+    # ex: /ebook-manager/123456790/ratings/
+    path('{}/<slug:pk>/ratings/'.format(label),  # route
          views.BookRatingsView.as_view(),           # view
          name='book_ratings'),
-    # ex: /ebook_manager/123456790/rate/
-    path('{}/<slug:book_id>/rate/'.format(app_name), views.rate, name='rate'),
+    # ex: /ebook-manager/123456790/rate/
+    path('{}/<slug:book_id>/rate/'.format(label), views.rate, name='rate'),
 ]

b85dc14cccd4edaa47848dc5b90eb1181394c2b0
Raul <rchfe23@gmail.com>
2020-09-26 19:48:14 -0400

    urls.py: use /author/ and /book/ in URLs and raise 404 error if no author name or book id in URLs

--- ebook_manager/urls.py
+++ ebook_manager/urls.py
@@ -1,27 +1,33 @@
+from django.http import Http404
 from django.urls import path, re_path
 
 from . import apps, views
 
 
+def response_error_handler(request):
+    raise Http404("Not found: {}".format(request.get_full_path_info()))
+
+
 app_name = apps.EbookManagerConfig.name
 label = apps.EbookManagerConfig.label
-# app_name = label
 urlpatterns = [
-    # ex: /ebook-manager/add_files/
+    # ex: /ebook-manager/upload_files/
     path('{}/upload_files/'.format(label), views.upload_files, name='upload_files'),
     # ex: /ebook-manager/
     # the 'name' value as called by the {% url %} template tag
     re_path(r'^$', views.IndexView.as_view(), name='index'),
     re_path(r'^{}/$'.format(label), views.IndexView.as_view(), name='index'),
+    path('{}/author/'.format(label), response_error_handler),
+    path('{}/book/'.format(label), response_error_handler),
     # ex: /ebook-manager/123456790/
     # TODO: slug?
-    path('{}/<slug:pk>/'.format(label),  # route
-         views.BookDetailView.as_view(),    # view
+    path('{}/book/<slug:pk>/'.format(label), views.BookDetailView.as_view(),
          name='book_detail'),
     # ex: /ebook-manager/123456790/ratings/
-    path('{}/<slug:pk>/ratings/'.format(label),  # route
-         views.BookRatingsView.as_view(),           # view
+    path('{}/book/<slug:pk>/ratings/'.format(label), views.BookRatingsView.as_view(),
          name='book_ratings'),
     # ex: /ebook-manager/123456790/rate/
-    path('{}/<slug:book_id>/rate/'.format(label), views.rate, name='rate'),
+    path('{}/book/<slug:book_id>/rate/'.format(label), views.rate, name='rate'),
+    path('{}/author/<slug>/'.format(label), views.AuthorDetailView.as_view(),
+         name='author-detail'),
 ]
